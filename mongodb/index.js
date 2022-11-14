bbc68e40e90ae9f5fa17cf94d86b473a52de21d4
yohanesanjar <yohanesanjar@gmail.com>
2022-11-14 20:24:30 +0700

    tpa-5

--- mongodb/index.js
+++ mongodb/index.js
@@ -9,7 +9,7 @@ function connectMongoDb() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', () => {
-      console.log('BERHASIL CONNECT KE DATABASE!');
+      console.log('Connect to DB success!');
     });
 }
 
